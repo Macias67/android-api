9c360c73263b5b8d24d712dca2db2ed2b8897f5b
Luis Macias <luismacias.angulo@gmail.com>
2016-08-23 21:17:57 -0500

    Quite el uso del JWT

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,8 +14,8 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api\v1'], function ($a
 	$api->post('registro/fb', ['uses' => 'Auth@registerFB']);
 	$api->post('auth', ['uses' => 'Auth@authenticate']);
 
-	$api->group(['middleware' => 'jwt.auth'], function($api)
-	{
+//	$api->group(['middleware' => 'jwt.auth'], function($api)
+//	{
 		$api->get('usuarios/me', 'Auth@me');
 		$api->get('validatetoken', 'Auth@validateToken');
 
@@ -37,5 +37,5 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api\v1'], function ($a
 		 * Tags
 		 */
 		$api->resource('tags', 'Tags');
-	});
+//	});
 });
